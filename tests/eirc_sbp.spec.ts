1ee4844e6c5b12b57b0b9d47305db59b2dfe3e5c
Bogdanchik88 <Bogdanchebotarev@gmail.com>
2026-03-31 09:59:32 +0300

    started making tests for eirc

---
+++ tests/eirc_sbp.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test('eirc_readings', async({page}) => {
+    const login = process.env.eirc_login_number
+    const password = process.env.eirc_login_password
+
+    if (!login || !password) {
+        throw new Error('Не заданы tgc1_login_email или tgc1_password в .env');
+    }
+
+    await page.goto('https://ikus.pesc.ru/auth/login')
+
+    const inputs = await page.getByRole('textbox')
+    await expect(inputs).toHaveCount(2)
+
+    await inputs.first().fill(login)
+    await inputs.nth(1).fill(password)
+
+    await page.getByRole('button', { name: 'Войти', exact: true }).click();
+})
